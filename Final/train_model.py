5515c55c5357b48486ff82da7ee25caca4a5bee4
David <davidlin@live.nl>
2024-05-04 11:17:10 +0200

    added fix

--- Final/train_model.py
+++ Final/train_model.py
@@ -8,7 +8,7 @@ from evaluate import evaluate
 
 
 def train_model(policy, n_training_episodes, eval_steps, env, eval_env, val_func=None,
-                bootstrap=False, baseline_subtraction=False, gamma=0.99, n=1, eta_entropy=0.01, verbose=False):
+                bootstrap=False, baseline_subtraction=False, gamma=0.99, n=1, eta_entropy=0.01, verbose=True):
     """
     Trains the model.
     takes policy and return eval_rewards and eval_episodes lists.
@@ -27,58 +27,51 @@ def train_model(policy, n_training_episodes, eval_steps, env, eval_env, val_func
         rewards = []
         entropies = []
         state, _ = env.reset()
-
         budget = 0
-        while True or budget < max_n_timesteps:
-            states.append(state)
+        done = False
+        while not done or budget < max_n_timesteps:
             action, log_prob, entropy = policy.act(state)
             saved_log_probs.append(log_prob)
-            state, reward, done, truncated, _ = env.step(action)
-            rewards.append(reward)
             entropies.append(entropy)
+
+            next_state, reward, done, truncated, _ = env.step(action)
+            states.append(state)
+            rewards.append(reward)
+
             if done or truncated:
                 break
+            state = next_state
             budget += 1
 
         returns = deque(maxlen=max_n_timesteps)
         T = len(rewards)
-
         val = 0
+
         # calculate the returns from T-1 to 0
-        for t in range(T)[::-1]:
-            if bootstrap and val_func is not None:
-                val = 0
-                if t + n >= T:
-                    n2 = T - t
-                    for i in range(n2):
-                        val += rewards[t + i] * pow(gamma, i)
-                    # since our episodes always have a terminal state, dont use critic at final state
-                else:
-                    for i in range(n):
-                        val += rewards[t + i] * pow(gamma, i)
-                    val += val_func.critic(torch.from_numpy(np.array(states[t + n])).float().unsqueeze(0)) * pow(gamma,
-                                                                                                                 n)
+        for t in range(T-1, -1, -1):
+            if bootstrap and t + n < T:
+                future_state = torch.from_numpy(np.array(states[t + n])).float().unsqueeze(0)
+                val = val_func.critic(future_state) * pow(gamma, n)
+                val = sum(gamma ** i * rewards[t + i] for i in range(n)) + val
             else:
                 # calculate the returns from T-1 to 0
                 val = gamma * val + rewards[t]
 
             # baseline subtraction
-            if baseline_subtraction and val_func is not None:
-                val -= val_func.critic(torch.from_numpy(np.array(states[t])).float().unsqueeze(0))
-            returns.appendleft([val])
+            if baseline_subtraction:
+                returns.appendleft([val - val_func.critic(torch.from_numpy(np.array(states[t])).float().unsqueeze(0))])
+            else:
+                returns.appendleft([val])
 
-
-
-        returns = torch.tensor(returns)
+        returns_tensor = torch.tensor(returns)
         # returns is either discounted returns, Q_sa or A_sa
 
         # if critic, train critic
         if val_func is not None and (bootstrap or baseline_subtraction):
-            val_func.train(states, returns)
+            val_func.train(states, returns_tensor)
 
         # call policy train
-        policy.train(saved_log_probs, returns, entropies, eta_entropy)
-
+        policy.train(saved_log_probs, returns_tensor, entropies, eta_entropy)
 
         # call evaluate if eval_steps interval reached
         if i_episode % eval_steps == 0:
